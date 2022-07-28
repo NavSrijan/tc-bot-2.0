0d37473da06282a0784de59d698726e2ea0e5e10
Nav Srijan <nav.srijan.official@gmail.com>
2022-07-28 13:03:52 +0530

    updated links

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def on_message(message: discord.Message):
                 to_pass = False
                 break
         if to_pass==True:
-            for i in ["https://cdn.discordapp.com/attachments", "https://imgur.com", "https://i.imgur.com", "https://images-ext-1.discordapp.net/external"]:
+            for i in ["https://media.discordapp.net/attachments","https://cdn.discordapp.com/attachments", "https://imgur.com", "https://i.imgur.com", "https://images-ext-1.discordapp.net/external"]:
                 if i in message.content:
                     #await message.author.send(f"You aren't allowed to send link in <#{os.environ['revive_channel']}>.")
                     await message.author.send(f"You aren't allowed to send link in <#{957263189320540170}>.")
