3fd620561cac2e92be534e090c14697def90802f
Nav Srijan <nav.srijan.official@gmail.com>
2022-08-15 08:46:54 +0530

    Added freedom.py

---
+++ cogs/freedom.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+import os
+import ipdb
+
+
+class Freedom(commands.Cog):
+    """Basic hello commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="how", aliases=["how's", "hows", "htj"])
+    async def Josh(self, ctx, *args):
+        """JOSH!"""
+        ipdb.set_trace()
+        alia=["how's the josh", "hows the josh?", "how's the josh?", "htj?", "htj"]
+        for i in alia:
+            if ctx.message.content[1:].lower().startswith(i):
+                await ctx.send("**HIGH SIR!!!**")
+                return
+
+
+async def setup(bot):
+    await bot.add_cog(Freedom(bot))
