531c9924ee38abd27a658a3e71d81d848f969984
Nav Srijan <nav.srijan.official@gmail.com>
2022-08-21 00:09:37 +0530

    Tried fixing suggestions

--- helpers.py
+++ helpers.py
@@ -7,7 +7,6 @@ from functions import download_and_return_image
 from io import BytesIO
 import textwrap
 
-
 class PersistentView(discord.ui.View):
 
     def __init__(self):
@@ -62,7 +61,7 @@ class VoteView(discord.ui.View):
             suggs = desc.split("\n")[0]
             up = len(db.fetch_interactions_id(interaction.message.id, 1))
             down = len(db.fetch_interactions_id(interaction.message.id, 0))
-            emb.description = "\n\n".join([suggs, lines.format(up, down)])
+            emb.description = suggs+"\n\n"+lines.format(up, down)#"\n\n".join([suggs, lines.format(up, down)])
 
             await interaction.response.edit_message(embed=emb)
         except Exception as e:
@@ -88,7 +87,7 @@ class VoteView(discord.ui.View):
             suggs = desc.split("\n\n")[0]
             up = len(db.fetch_interactions_id(interaction.message.id, 1))
             down = len(db.fetch_interactions_id(interaction.message.id, 0))
-            emb.description = "\n".join([suggs, lines.format(up, down)])
+            emb.description = suggs+"\n\n"+lines.format(up, down)
 
             await interaction.response.edit_message(embed=emb)
         except Exception as e:
